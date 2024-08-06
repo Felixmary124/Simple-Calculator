f799267583203ca194839fbde0db86177c3e42ae
Felix Ajaebili <felixmary124@gmail.com>
2024-08-06 14:11:12 +0200

    first commit

---
+++ calculator.js
@@ -0,0 +1,21 @@
+let result = document.getElementById("inputext");
+
+let calculate = (number) => {
+  result.value += number;
+};
+
+let Result = () => {
+  try {
+    result.value = eval(result.value);
+  } catch (err) {
+    alert("Enter the valid Input");
+  }
+};
+
+function clr() {
+  result.value = " ";
+}
+
+function del() {
+  result.value = result.value.slice(0, -1);
+}
